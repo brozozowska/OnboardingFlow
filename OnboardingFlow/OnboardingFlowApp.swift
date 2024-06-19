a6b689d51f2db6b31d40b482105bf0913b1d3da0
brozozowska <s_rozov@me.com>
2024-06-19 16:16:49 +0300

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Сергей Розов on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
